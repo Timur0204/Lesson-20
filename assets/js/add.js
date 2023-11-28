42e46ae4ae098013d5ced4d3d62f5886d69b4c54
Timur0204 <timur02042008@gmail.com>
2023-11-28 09:05:31 +0200

    maybe the last fix

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,30 +1,31 @@
 
-   let sum = +prompt('Введите сумму кредита: (в грн).');
-   console.log(`Сумма кредита: ${sum} грн.`);
-   let term = +prompt('Введите срок кредитования: (месяцев).');
-   console.log(`Срок кредитования: ${term} месяцев`);
-   let rate = +prompt('Укажите процентную ставку (% годовых)');
-   console.log(`Процентная ставка: ${rate} % годовых`)
+let sum = +prompt('Введите сумму кредита: (в грн).');
+console.log(`Сумма кредита: ${sum} грн.`);
+let term = +prompt('Введите срок кредитования: (месяцев).');
+console.log(`Срок кредитования: ${term} месяцев`);
+let rate = +prompt('Укажите процентную ставку (% годовых)');
+console.log(`Процентная ставка: ${rate} % годовых`)
 
-   let mRate = rate / 12;
+let mRate = rate / 12;
 
-   let creditBodyInMonth = Math.round((sum / term) * 100) / 100;
-   console.log(`Тело кредита в месяц: ${creditBodyInMonth} грн.`);
+let creditBodyInMonth = Math.round((sum / term) * 100) / 100;
+console.log(`Тело кредита в месяц: ${creditBodyInMonth} грн.`);
 
-   let totalOverpayment = 0;
+let totalOverpayment = 0;
 
-   for (let i = 1; i <= term; i++) {
-    let interestPayment = Math.round((sum * mRate) / 100 * 100) / 100;
-    let loanRepayment = Math.round((creditBodyInMonth + interestPayment) * 100) / 100;
-    totalOverpayment = Math.round((totalOverpayment + interestPayment) * 100) / 100;
-    sum = Math.round((sum - creditBodyInMonth) * 100) / 100;
+for (let i = 1; i <= term; i++) {
+   let interestPayment = Math.round((sum * mRate / 100) * 100) / 100;
+   let loanRepayment = Math.round((creditBodyInMonth + interestPayment) * 100) / 100;
 
-    if (i === term && sum !== 0) {
-      sum = 0;
-      creditBodyInMonth = creditBodyInMonth + sum;
-      loanRepayment = loanRepayment + sum;
-    }
-    console.log(`#${i} Платеж(в месяц): ${loanRepayment} грн. Тело: ${creditBodyInMonth} грн, Проценты: ${interestPayment} %, Остаток долга: ${sum} грн`);
+   if (i === term) {
+       creditBodyInMonth = Math.round(sum * 100) / 100;
+       loanRepayment = Math.round((creditBodyInMonth + interestPayment) * 100) / 100;
+   }
+
+   totalOverpayment = Math.round((totalOverpayment + interestPayment) * 100) / 100;
+   sum = Math.round((sum - creditBodyInMonth) * 100) / 100;
+
+   console.log(`#${i} Платеж(в месяц): ${loanRepayment} грн, Тело: ${creditBodyInMonth} грн, Проценты: ${interestPayment} %, Остаток долга: ${sum} грн`);
 }
 
-   console.log(`Переплата по кредиту: ${totalOverpayment} грн`);
+console.log(`Переплата по кредиту: ${totalOverpayment} грн`);
